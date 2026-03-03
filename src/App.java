27b7d6c9f5d564885593ab33ae3bd9f62eb09eb4
Ed1kson <oplakanskiytitan@gmail.com>
2026-03-03 18:22:51 +0100

    task 1 done, getClassInfo method is ready

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import myUtility.*;
+import java.util.Scanner;
+
 public class App {
+    static Scanner in = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println(MyReflector.getClassInfo( "java.lang.String" ));
+        System.out.println(MyReflector.getClassInfo( "suka blyat" ));
     }
 }
---
+++ src/myUtility/MyReflector.java
@@ -0,0 +1,46 @@
+package myUtility;
+import java.lang.reflect.*;
+
+public class MyReflector {
+
+    public static String getClassInfo(String className) {
+        Class <?> cls;
+        try {
+            cls = Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Class not found: " + className);
+            return null;
+        }
+
+        return getClassInfo(cls);
+    }
+
+    public static String getClassInfo(Class<?> clazz) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Class Name: " + clazz.getSimpleName() + "\n");
+        sb.append("Package: " + clazz.getPackageName() + "\n");
+        sb.append("Modifiers: " + Modifier.toString(clazz.getModifiers()) + "\n");
+        sb.append("Superclass: " + clazz.getSuperclass().getName() + "\n");
+        sb.append("Constructors:\n");
+        for (Constructor<?> iface : clazz.getConstructors()) {
+            sb.append("  - " + iface.toString() + "\n");
+        }
+        sb.append("Fields:\n");
+        for (Field iface : clazz.getFields()) {
+            sb.append("  - " + iface.toString() + "\n");
+        }
+        sb.append("Interfaces:\n");
+        for (Class<?> iface : clazz.getInterfaces()) {
+            sb.append("  - " + iface.toString() + "\n");
+        }
+        sb.append("Methods:\n");
+        for (Method iface : clazz.getMethods()) {
+            sb.append("  - " + iface.toString() + "\n");
+        }
+    
+        return sb.toString();
+    }
+    
+    private MyReflector() { }
+
+}
