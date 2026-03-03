09a5dc83bc7f1d3ee4fa1911e5fadade8214b60c
Ed1kson <oplakanskiytitan@gmail.com>
2026-03-03 20:50:16 +0100

    task 2 done

--- src/App.java
+++ src/App.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 public class App {
     static Scanner in = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
+        task2();
+    }
+
+    public static void task1() {
        System.out.print("Enter the full name of the class: ");
        String str = MyReflector.getClassInfo(in.nextLine());
 
@@ -12,6 +16,11 @@ public class App {
         } else {
             System.out.println(str);
         }
-        
+    }
+
+    public static void task2() {
+        App a = new App();
+        String str = MyReflector.getObjectState(a);
+        System.out.println(str);
     }
 }
--- src/myUtility/MyReflector.java
+++ src/myUtility/MyReflector.java
@@ -21,22 +21,80 @@ public class MyReflector {
         sb.append("Modifiers: " + Modifier.toString(clazz.getModifiers()) + "\n");
         sb.append("Superclass: " + clazz.getSuperclass().getName() + "\n");
         sb.append("Constructors:\n");
-        for (Constructor<?> iface : clazz.getConstructors()) {
-            sb.append("  - " + iface.toString() + "\n");
+        for (Constructor<?> constructor : clazz.getConstructors()) {
+            sb.append("  - " + constructor.toString() + "\n");
         }
         sb.append("Fields:\n");
-        for (Field iface : clazz.getFields()) {
-            sb.append("  - " + iface.toString() + "\n");
+        for (Field field : clazz.getFields()) {
+            sb.append("  - " + field.toString() + "\n");
         }
         sb.append("Interfaces:\n");
         for (Class<?> iface : clazz.getInterfaces()) {
             sb.append("  - " + iface.toString() + "\n");
         }
         sb.append("Methods:\n");
-        for (Method iface : clazz.getMethods()) {
-            sb.append("  - " + iface.toString() + "\n");
+        for (Method method : clazz.getMethods()) {
+            sb.append("  - " + method.toString() + "\n");
         }
     
+        return sb.toString();
+    }
+    
+    public static String getObjectState(Object obj) {
+        Class<?> cls = obj.getClass();
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("fields: \n");
+        for ( Field field : cls.getDeclaredFields() ) {
+            field.setAccessible(true);
+            if ( field.getModifiers() == Modifier.STATIC ) {
+                try {
+                    sb.append("  - (static field)" + field.getName() + " = " + field.get(null) + "\n");
+                } catch (IllegalAccessException e) {
+                    sb.append("  - " + field.getName() + " = " + "ACCESS DENIED\n");
+                }
+            } else {
+                try {
+                    sb.append("  - " + field.getName() + " = " + field.get(obj) + "\n");
+                } catch (IllegalAccessException e) {
+                    sb.append("  - " + field.getName() + " = " + "ACCESS DENIED\n");
+                }
+            }
+        }
+
+        sb.append("methods: \n");
+        int i = 1;
+        for ( Method method : cls.getDeclaredMethods() ) {
+            // method.setAccessible(true);
+            sb.append("  " + i + ". " + method.getName() + "\n");
+            i++;
+        }
+
+        //task 2 stuff
+        System.out.println(sb.toString());
+
+        java.util.Scanner in = new java.util.Scanner(System.in);
+        int numMethods = cls.getDeclaredMethods().length;
+        System.out.printf("pick a method to invoke(%d-%d): \n", 1, numMethods);
+            int methodNum = in.nextInt();
+            in.nextLine(); // consume the newline
+    
+            if ( methodNum < 1 || methodNum > cls.getDeclaredMethods().length ) {
+                sb.append("invalid method number");
+                return sb.toString();
+            }
+    
+            Method methodToInvoke = cls.getDeclaredMethods()[methodNum - 1];
+            methodToInvoke.setAccessible(true);
+            try {
+                Object result = methodToInvoke.invoke(obj);
+                sb.append("result of invoking " + methodToInvoke.getName() + ": " + result + "\n");
+            } catch (Exception e) {
+                sb.append("failed to invoke method: " + e.getMessage() + "\n");
+            }
+        in.close();
+        // end of task 2 stuff
+
         return sb.toString();
     }
     
